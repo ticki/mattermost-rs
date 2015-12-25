d02ad970283209fbe307076435b785c09c707f45
Skyler Berg <skylertheberg@gmail.com>
2015-12-24 16:48:04 -0800

    Make URL not an option

--- src/server.rs
+++ src/server.rs
@@ -20,23 +20,22 @@ pub struct Server<C: OutgoingCallback> {
 }
 
 pub struct IncomingHook {
-    url: Option<&'static str>,
+    url: &'static str,
 }
 
 impl IncomingHook {
     pub fn new(url: &'static str) -> IncomingHook {
         IncomingHook {
-            url: Some(url),
+            url: url,
         }
     }
-    pub fn send(&self, payload: IncomingPayload) {
-        let url = self.url.expect("Client not initialized");
 
+    pub fn send(&self, payload: IncomingPayload) {
         let mut headers = Headers::new();
         headers.set(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![])));
 
         let client = Client::new();
-        let mut res = client.post(url)
+        let mut res = client.post(self.url)
                         .header(Connection::close())
                         .headers(headers)
                         .body(
